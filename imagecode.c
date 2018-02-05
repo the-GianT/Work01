806a6d0c75ac98ddd3c0cdc4c11328ae32764f99
Gian Tricarico <gtricarico@stuy.edu>
2018-02-05 15:27:04 -0500

    cleanup + add close statement

--- imagecode.c
+++ imagecode.c
@@ -10,15 +10,8 @@
 
 int main()
 {
-  /*
-  // for adding together to generate next fib #
-  int fib0 = 0;
-  int fib1 = 1;
-  */
-
   char buf[40]; // for writing to image file (starts as image header)
   
-  // int i;
   int ctr; // counts how many subpixels it has written
   int subpixelval; // integer to write
   char initchars[2]; // determines whether to put two spaces or a newline at the beginning of buf
@@ -32,7 +25,6 @@ int main()
 
   for (ctr = 0; ctr < NUMROWS * NUMCOLS; ctr++) {
     if (ctr % NUMCOLS == 0) { // if we are at the end of the row
-      // endchar = "\n";
       strcpy(initchars, "\n");
     } else {
       strcpy(initchars, "  ");
@@ -43,31 +35,6 @@ int main()
   }
 
   write(fd, "\n", sizeof(char));
-
-  /*
-  // fill in first two subpixels
-  sprintf(buf, "%d ", fib0);
-  write(fd, buf, strlen(buf) * sizeof(char));
-  sprintf(buf, "%d ", fib1);
-  write(fd, buf, strlen(buf) * sizeof(char));
-  
-  for (ctr = 2; ctr < NUMROWS * NUMCOLS; ctr++) {
-    if (ctr % (NUMROWS * 3) == 0) { // if we are at the end of the row
-      endchar = '\n';
-    } else {
-      endchar = ' ';
-    }
-    
-    if (fib0 < fib1) {
-      sprintf(buf, "%d%c", fib0 = fib0 + fib1, endchar);
-    } else {
-      sprintf(buf, "%d%c", fib1 = fib0 + fib1, endchar);
-    }
-    
-    write(fd, buf, strlen(buf) * sizeof(char));
-    // ctr ++;
-  }
-  */
-
+  close(fd);
   return 0;
 }
